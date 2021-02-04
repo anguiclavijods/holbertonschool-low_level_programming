33c27f271e16065d93cd2a21aaa6215094b91b1b
AnguiDev <angiedesarrollo@gmail.com>
2021-02-04 15:01:29 -0500

    correction file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
-#inclide <time.h>
+#include <time.h>
+#include <studio.h>
 /*
+ * main - clasify an integer 
  *
- *
+ * Return: Always 0 (Success)
  */
 int main (void)
 {
 	int n;
 	srand(time(0));
 	n=rand() - RAND_MAX/2;
-	//
+	if(n>0)
+		print ("%d is positive\n",n);
+	else if(n<0)
+		print("%d is negative\n",n);
+	else if(n==0)
+		printf("%d is zero\n",n);
 	return (0);
 }
