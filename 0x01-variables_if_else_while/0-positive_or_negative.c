06044973be8f57f283e0d0c7b676f8caaeb82a5c
AnguiDev <angiedesarrollo@gmail.com>
2021-02-04 17:08:43 -0500

    correct

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,44 @@
 #include <stdlib.h>
 #include <time.h>
-#include <studio.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
- * main - clasify an integer
+ *main - it is where the program start
  *
- * Return: Always 0 (Success)
+ *Description: a random number is generated and at the end
+ *it prints if is positive, negative or zero
+ *Return: positive or negative
  */
+
+
 int main(void)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		print ("%d is positive\n", n);
-	else if (n < 0)
-		print("%d is negative\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	return (0);
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else
+{
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+}
+}
+return (0);
 }
