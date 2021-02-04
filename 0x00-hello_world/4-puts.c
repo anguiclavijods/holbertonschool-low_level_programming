2b30ca2e51551cea180ab9625f728a91c83e44e3
AnguiDev <angiedesarrollo@gmail.com>
2021-02-03 20:05:26 -0500

    puts message exact

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <studio.h>
 
-int main (void)
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle);
-    return (0);
+puts("\"Programming is like building a multilingual puzzle);
+return(0);
 }
