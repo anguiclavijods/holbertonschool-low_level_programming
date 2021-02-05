afadbc586cb898f158216078578a9e3760bc127e
AnguiDev <angiedesarrollo@gmail.com>
2021-02-04 22:59:19 -0500

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,22 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - last digit
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastDigit = n % 10;
-	printf("Last digit of %d ", n);
-	printf("is %d ", lastDigit);
-	if (lastDigit > 5)
-		printf("and is greater than 5");
-	else if (lastDigit == 0)
-		printf("and is 0");
+	printf("Last digit of %i is %i ", n, (n % 10));
+	if ((n % 10) > 5)
+		printf("and is greater than 5\n");
+	else if ((n % 10) == 0)
+		printf("and is 0\n");
 	else
-		printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-project it if
+holbertonschool-low_level_programming
