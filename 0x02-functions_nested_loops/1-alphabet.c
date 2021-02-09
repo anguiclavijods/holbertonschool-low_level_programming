762f2662728138762de4077a13f030c33d04cad3
AnguiDev <angiedesarrollo@gmail.com>
2021-02-09 14:57:45 -0500

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char = a;
+
+	for  (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n');
+	return (0);
+}
+
