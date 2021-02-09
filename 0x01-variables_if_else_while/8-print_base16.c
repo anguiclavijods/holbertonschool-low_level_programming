3ce00df196685bab57a8100ba1a2e4b849a20c41
AnguiDev <angiedesarrollo@gmail.com>
2021-02-08 22:50:53 -0500

    prints all the numbers base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+/**
+ * main - Print the numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + 48);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
