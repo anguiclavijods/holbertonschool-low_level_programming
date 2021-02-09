20c72a064ff0fc861f444a1c6471357f62c39a24
AnguiDev <angiedesarrollo@gmail.com>
2021-02-09 17:04:19 -0500

    alphabet *10

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isalpha - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+
+	if (c)
+	{
+		_putchar('');
+		reurn (1);
+	}
+	else if ()
+	{
+		return(0);
+	}
