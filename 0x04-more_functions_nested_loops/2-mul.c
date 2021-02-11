08f3ee2acd04a5f8e2d60b4b5fd46848e1130bb7
AnguiDev <angiedesarrollo@gmail.com>
2021-02-11 16:16:56 -0500

    multiples two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - function that multiplies two integers.
+ *@a: multiples two integers
+ *@b: multiple two integers
+ *Return: result multiples a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
