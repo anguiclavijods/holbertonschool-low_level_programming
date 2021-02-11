5581d706200f7361e603170989b51075f015d824
AnguiDev <angiedesarrollo@gmail.com>
2021-02-10 20:32:49 -0500

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @n print last number
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n);
+{
+	if (n > 0 && % n)
+	return (n);
+}
