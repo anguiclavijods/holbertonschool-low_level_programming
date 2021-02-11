c744e20c5e88dfa4df589aba0a2b306dd4d55007
AnguiDev <angiedesarrollo@gmail.com>
2021-02-10 19:31:15 -0500

    value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of a integer
+ *
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+	int r;
+
+	r = _abs(9);
+	_putchar("%d", r);
+	return (0);
+}
