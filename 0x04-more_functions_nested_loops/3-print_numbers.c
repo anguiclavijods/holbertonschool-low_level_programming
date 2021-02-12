6643008130e8697ea90af213dc4ec88688f039c9
AnguiDev <angiedesarrollo@gmail.com>
2021-02-11 19:39:43 -0500

    print numbers 0 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
+
 #include "holberton.h"
+
 /**
- * print_numbers - print the numbers
- * void: none args
+ * print_numbers - prints the numbers, from 0 to 9
  *
+ * Return: None
  */
 void print_numbers(void)
 {
-	int a;
+	int i;
 
-	a = 48;
-	while (a <= 57)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(a '\n');
-		a++;
+		_putchar('0' + i);
 	}
+		_putchar('\n');
 }
