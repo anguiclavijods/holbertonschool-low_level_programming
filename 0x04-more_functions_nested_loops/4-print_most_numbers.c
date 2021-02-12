b3dc6d433ab3bd581d789b42de52a95f8c0aa617
AnguiDev <angiedesarrollo@gmail.com>
2021-02-12 17:21:19 -0500

    print numbers 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,13 @@
 void print_most_numbers(void)
 {
 	char c;
+
 	c = '0';
-	
 	while (c <= '9')
 	{
-	     if(c != '2' || c != '4'){
-		_putchar(c);     
-	     }
-
-	     c++;
+		if (c != '2' || c != '4')
+			_putchar (c);
+		c++;
 	}
 	_putchar('\n');
-
-	
 }
