3e6ff8e5018d9fe515c2808abbd94ba39ce4e70d
AnguiDev <angiedesarrollo@gmail.com>
2021-02-13 00:02:05 -0500

    print numbers  0 9 not 2 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_most_number - prints the numbers, 0 to 9 except 2 and 4
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4
  *
  * Return: None
  */
 void print_most_numbers(void)
 {
-	char c;
+	int i;
 
-	c = '0';
-	while (c <= '9')
+	for (i = 0; i <= 9; i++)
 	{
-		if (c != '2' || c != '4')
-			_putchar (c);
-		c++;
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
