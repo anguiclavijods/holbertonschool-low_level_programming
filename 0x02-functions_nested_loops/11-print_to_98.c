40faa787a873acb6f7b14b7cf7f2e41417c52cb6
AnguiDev <angiedesarrollo@gmail.com>
2021-02-15 12:13:40 -0500

    numbers of 0 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,9 +5,18 @@
  *Return: void
  */
 void print_to_98(int n)
-	n = '0';
 {
-	for (n >= '0'; n <= '98'; n++)
-		_putchar(n);
-	return (0);
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+		{
+			n--;
+		}
+		else if (n < 98)
+		{
+			n++;
+		}
+	}
+	printf("%i\n", 98);
 }
