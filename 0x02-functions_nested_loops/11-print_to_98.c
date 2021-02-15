26c02c19b82eb8ee42fe45387d2e1ec02ac500db
AnguiDev <angiedesarrollo@gmail.com>
2021-02-15 11:52:09 -0500

    numbers of 0 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *print_to_98 - print number of 0 to 98
+ *@n: int numbers 0
+ *Return: void
+ */
+void print_to_98(int n)
+	n = '0';
+{
+	for (n >= '0'; n <= '98'; n++)
+		_putchar(n);
+	return (0);
+}
