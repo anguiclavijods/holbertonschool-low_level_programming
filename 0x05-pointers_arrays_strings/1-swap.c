50ed525ee9c68ef7a6f9d27d81804417ad261807
AnguiDev <angiedesarrollo@gmail.com>
2021-02-16 15:57:02 -0500

    update value of n

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  *swap_int - swaps the values.
- *@a: value swaps
- *@b: value swaps
+ *@a: value swaps.
+ *@b: value swaps.
  *Return: none.
  */
 void swap_int(int *a, int *b)
