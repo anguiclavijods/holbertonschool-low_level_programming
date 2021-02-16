78cf434b57bee3e6f8c5ebf049291fc826b73f8e
AnguiDev <angiedesarrollo@gmail.com>
2021-02-15 22:19:08 -0500

    reset value of n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - main fuction. 
+ *@n: Variable and pointer
+ *reset value of pointer n to 98
+ *Return: none
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+	
