74f8e2b0c9f8a2c64f5b22177e769d729f0d1e67
AnguiDev <angiedesarrollo@gmail.com>
2021-02-17 18:37:19 -0500

    print numbers of 2 in 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,8 @@
 void puts2(char *str)
 {
 	int count = 0;
-	for (; *(str+count)  != '\0';  count += 2)
-		_putchar(*(str+count));
+
+	for (; *(str + count)  != '\0';  count += 2)
+		_putchar(*(str + count));
 	_putchar('\n');
 }
