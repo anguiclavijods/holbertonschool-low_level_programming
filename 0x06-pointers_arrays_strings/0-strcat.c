017948d4f20b69ab672cf90ca18281f23a1781d2
AnguiDev <angiedesarrollo@gmail.com>
2021-02-18 18:10:58 -0500

    function that concatenates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *_strcat - function that concatenate two strings
+ *@dest: value of dest
+ *@src: value of beging
+ *Return: none
  */
 char *_strcat(char *dest, char *src)
 {
 	char c;
+
 	c = *src;
 	src = dest;
 	dest = c;
