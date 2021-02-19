12bb877ea62840e87526868d400d66aaaa394ae0
AnguiDev <angiedesarrollo@gmail.com>
2021-02-19 00:01:47 -0500

    strings of pocision n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,24 @@
 #include "holberton.h"
 /**
- *
- *
+ *_strncat - string of pocision to n
+ *@dest: array dest
+ *@src: array src
+ *@n: pocision
+ *Return: value of dest
  */
 char *_strncat(char *dest, char *src, int n)
- {
-	 int i = 0;
-	 int j;
-	 for (; dest[i] != '\0'; i++)
-	 {
-		 
-	 }
-	 for (j = 0; j < n; j++)
-	 {
-		 dest[i] = src[j];
-		 i++;
-	 }
-	 dest =[i] '\0';
-	 return (dest);
- }
+{
+	int i = 0;
+	int j;
+
+	for (; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
