e906516e236e2f6b9429f536c2a96e9def0c49a0
AnguiDev <angiedesarrollo@gmail.com>
2021-02-20 19:05:16 -0500

    print table of multiplicate from 0 to table 9

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *print_times_table -function that prints the n times table, starting with 0.
+ *@n: value of x number
+ *Return: None
+ *
+ */
+void print_times_table(int n)
+{
+	if (n > 15 && n < 0)
+	{
+
+	}
+}
