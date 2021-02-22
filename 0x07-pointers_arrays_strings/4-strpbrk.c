f8e6750285670cc3349ace5a144af99c7e4c680b
AnguiDev <angiedesarrollo@gmail.com>
2021-02-22 18:55:59 -0500

    function search a string of a set bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- *
- *
- *
+ *_strpbrk - functions search a string of set bytes
+ *@s: pointer to char s
+ *@accept: value a remplace
+ *Return: Null
  */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i, j = 0;
+
+	for(; s[i] != '\0'; i++)
+	{
+		for(; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+			{   
+			}
+		}
+		
+	}
+	return (NULL);
 }
