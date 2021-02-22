0e274c356d6ce95ea41f16059491509791bed87b
AnguiDev <angiedesarrollo@gmail.com>
2021-02-22 18:13:42 -0500

    gets the length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,25 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *_strspn: function that gets the length of a prefix substring.
+ *@s: value of array
+ *@accept: value to remplace
+ *Return: value count.
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count;
+
 	j = count = 0;
 
-	for (i=0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (accept [j] == '\0')
+		if (accept[j] == '\0')
 		{
 			return (count);
 		}
-		for (j=0; accept [j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept [j] == s[i])
+			if (accept[j] == s[i])
 			{
 				count += 1;
 				break;
