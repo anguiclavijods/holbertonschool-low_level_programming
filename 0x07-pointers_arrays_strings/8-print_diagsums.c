f533be29fc10be7208d030b55822cd24f53cccd5
AnguiDev <angiedesarrollo@gmail.com>
2021-02-23 00:06:22 -0500

    sum diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,15 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
+ *print_diagsums - prints the sum of the two diagonals of a square matrix of integers.
+ *@a: value array
+ *@size: value ramplace
+ *Return: None.
  *
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
+	int i = 0, j = 0, c =0, c2 = 0;
 
-	for (; a[i] != size; i++)
+	for (; a[i] < size; i++)
 	{
-		a[i] += size;
+		j = i % size;
+		c += a [j +(i*size)];
+		c2 += a[i + ((size - 1 - i) * size)];
 	}
+	printf("%d, %d\n", c, c2);
 }
