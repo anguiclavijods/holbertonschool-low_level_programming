9febcd849461467dba59976ac01e6d6054491bfd
AnguiDev <angiedesarrollo@gmail.com>
2021-02-24 11:00:45 -0500

    update git ignore with files main.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  *print_times_table -function that prints the n times table, starting with 0.
  *@n: value of x number
@@ -7,8 +7,21 @@
  */
 void print_times_table(int n)
 {
-	if (n > 15 && n < 0)
-	{
+	int i, j, opera1, opera2, opera3 = 0;
 
+	while (n < 0 && n > 15)
+	{
+		n++;
+	}
+	while(i <= n)
+	{
+		while (j <= n)
+		{
+			opera1 = ( i * j / 100) % 10;
+			opera2 = ( i * j / 10) % 10;
+			opera3 = ( i * j ) / 1 % 10;
+		j++;
+		}
+	i++;
 	}
 }
