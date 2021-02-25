d9af0f54823948311a84d0548b6ebf4aaafe6920
AnguiDev <angiedesarrollo@gmail.com>
2021-02-25 15:45:52 -0500

    number is a prime or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,34 @@
 #include "holberton.h"
 /**
- *
- *
+ *test - new function
+ *@i: new variable
+ *@j: new second variable that verify the value of n
+ *Return: value of i and j
  */
-int test (int n, int a)
+int test(int i, int j)
 {
-	if ( n >= 2)
-	{
-		a++;
-	}
-	return (a);
-}
-int is_prime_number(int n)
-{
-	
-	if ( n <= 1)
+	if (i % j == 0 || (i <= 1))
 	{
 		return (0);
 	}
-		
+	if (j > i / 2)
+	{
+		return (1);
+	}
 	else
 	{
-		if (n % test (a) == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return(1);
-		}
-		
+		return (test(i, j + 1));
 	}
-	
+}
+
+
+/**
+ *is_prime_number - function for numbers prime
+ *@n: value to verify if is a prime
+ *Return: value of n is prime 1 otherwise 0
+ *
+ */
+int is_prime_number(int n)
+{
+	return (test(n, 2));
 }
