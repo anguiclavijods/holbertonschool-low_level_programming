b680064e6cede42f1a11e48f618870a73402c55c
AnguiDev <angiedesarrollo@gmail.com>
2021-02-27 18:23:48 -0500

    created library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
 * _puts -  print a string
-* @str: string
+* @s: string
 * Return: none
 */
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str != '\0')
+	while (*s != '\0')
 	{
-		_putchar (*str);
-		str++;
+		_putchar (*s);
+		s++;
 	}
 	_putchar ('\n');
 }
