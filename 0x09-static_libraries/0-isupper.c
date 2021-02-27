26f510105c96d114bdfed6ca22bea4baf15a1147
AnguiDev <angiedesarrollo@gmail.com>
2021-02-27 15:56:20 -0500

    created library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ *_putchar - print char
+ *@c: variable type char
+ *Return: none.
+ */
 int _putchar(char c)
 {
 
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ *_islower - function
+ *@c: variable type int
+ *Return: none.
+ */
 int _islower(int c)
 {
 
