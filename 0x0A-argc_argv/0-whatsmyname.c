dc8a741462a6b27343d2faf8eb975e3b7ffd7f53
AnguiDev <angiedesarrollo@gmail.com>
2021-02-27 21:07:10 -0500

    printf myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-#include "stdio.h"
+#include <stdio.h>
 /**
  *main - function start
  *@argc: variable type int
@@ -11,8 +10,6 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+	printf("%s\n", argv[i]);
 	return (0);
 }
