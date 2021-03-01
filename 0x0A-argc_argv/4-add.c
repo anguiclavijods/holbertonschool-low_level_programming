4417b59cf51974afa0d8957a05e5d82bb8ea2a72
AnguiDev <angiedesarrollo@gmail.com>
2021-02-28 22:07:24 -0500

    addition number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,43 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
-int i = 1, result = 0,  content= 0;
-char *consta;
-if (argc == 1)
-        {
-        printf("0\n");
-        return (0);
-        }
-do {
-        consta = argv[i];
-        if (*consta  == 48)
-                {
-                result += 0;
-                }
-        else
-                {
-                content = atoi(argv[i]);
-                        if (content == 0)
-                                {
-                                printf("Error\n");
-                                return (1);
-                                }
-                        else
-                                result += content;
-                }
-        i++;
-} while (i < argc);
-printf("%d\n", result);
-return (0);
-}
+	int i = 1, result = 0,  content= 0;
+	char *consta;
 
-
-/*	int result, i = 1;
-
-	if (argc > 0 )
+	if (argc == 1)
 	{
-		while (argv[i] != '\0')
-		result = atoi(argv[1]) + atoi(argv[2]);
-		printf("%d\n", result);
+		printf("0\n");
+		return (0);
 	}
-	return (0);*/
+	do
+	{
+		consta = argv[i];
+		if (*consta  == 48)
+		{
+			result += 0;
+		}
+		else
+		{
+			content = atoi(argv[i]);
+			if (content == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				result += content;
+		}
+		i++;
+	}
+	while (i < argc);
+	printf("%d\n", result);
+	return (0);
+}
