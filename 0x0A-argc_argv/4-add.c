5456271d1ffd7266b12c963d7f6b980e547ebb60
AnguiDev <angiedesarrollo@gmail.com>
2021-02-28 23:06:14 -0500

    adding numbers postive

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - main funtion that print of addition of numbers
  * @argc: integer
@@ -8,35 +9,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1, result = 0,  content= 0;
-	char *consta;
+	int i, j, result;
 
-	if (argc == 1)
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result = result + atoi(argv[i]);
+		}
+	}
+	else
 	{
 		printf("0\n");
 		return (0);
 	}
-	do
-	{
-		consta = argv[i];
-		if (*consta  == 48)
-		{
-			result += 0;
-		}
-		else
-		{
-			content = atoi(argv[i]);
-			if (content == 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-				result += content;
-		}
-		i++;
-	}
-	while (i < argc);
-	printf("%d\n", result);
+	printf("%i\n", result);
 	return (0);
 }
