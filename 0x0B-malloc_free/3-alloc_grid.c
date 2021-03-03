1ef0f697272547d2f19bddecb704075c3296a603
AnguiDev <angiedesarrollo@gmail.com>
2021-03-03 18:08:28 -0500

    function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,37 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * 
- * 
- * 
+ * alloc_grid - Name function
+ * @width: whidth array
+ * @height: height array
+ * Return: Value array
  */
- int **alloc_grid(int width, int height)
- {
-     int  **intarray, i;
+int **alloc_grid(int width, int height)
+{
+	int  **intarray, i;
 
-     if (width <= 0 || height <= 0)
-     {
-        return (NULL);
-     }
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-    intarray = malloc(sizeof(int*)* height);
+	intarray = malloc(sizeof(int *) * height);
 
-    if (intarray == NULL)
-    {
-        return (NULL);
-    }
-
-    for (i = 0; i < height; i++)
-    {
-        intarray[i] = malloc(sizeof(int) * width);
-
-        if (intarray[i] == NULL)
-        {
-            for ( ; i  != 0; i--)
-            {
-                free(intarray[i]);
-            }
-            free(intarray);
-            return(NULL);
-        }
-        
-    }
-    return (intarray);
- }
+	if (intarray == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		intarray[i] = malloc(sizeof(int) * width);
+		if (intarray[i] == NULL)
+		{
+			for ( ; i  != 0; i--)
+			{
+				free(intarray[i]);
+			}
+			free(intarray);
+			return (NULL);
+		}
+	}
+	return (intarray);
+}
