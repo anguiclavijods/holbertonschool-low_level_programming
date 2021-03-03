86d4d35c21fc07267381566343b39b37a55f6384
AnguiDev <angiedesarrollo@gmail.com>
2021-03-03 13:45:38 -0500

    exercise 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * 
+ * 
+ */
+char *argstostr(int ac, char **av)
+{
+    char *chara;
+    int i, j;
+
+    if( ac == 0 && av == NULL)
+    {
+        return(NULL);
+    }
+    if(!chara)
+    {
+        return(NULL);
+    }
+    chara = malloc(sizeof(char)*ac);
+    {
+        if (chara == NULL)
+        {
+            return(NULL);
+        }
+    }
+    for ( i = 0; i < ac; i++)
+    {
+        for ( j = 0; av[i][j] !='\0'; j++)
+        {
+        }
+        chara[i] = malloc(sizeof(char) * (j + 1));
+        
+    }
+    return (chara);
+ 
+}
