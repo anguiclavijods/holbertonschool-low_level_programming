e8717bb180596965a3dd6071fc29251e97d79c1a
AnguiDev <angiedesarrollo@gmail.com>
2021-03-04 00:21:43 -0500

    exercise 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,45 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- * 
+ * argstostr - name function
+ * @ac: size of array
+ * @av: size
+ * Return: Value of pointer
  */
 char *argstostr(int ac, char **av)
 {
-    char *chara;
-    int i, j, size = 0, count = 0;
-    
-    if( ac == 0 && av == NULL)
-    {
-        return(NULL);
-    }
-    /*chara = malloc(sizeof(chara)*ac);
-    {
-       
-        
-    }*/
-    for ( i = 0; i < ac; i++)
-    {
-        for ( j = 0; av[i][j] !='\0'; j++)
-        {
-            size++;
-        }
-        size++;
+	char *chara;
+	int i, j, size = 0, count = 0;
 
-        chara = malloc(size *sizeof(char));
-         if (chara == NULL)
-        {
-            return(NULL);
-        }
-        for ( i = 0; av[i][j] !='\0'; i++)
-        {
-            chara[count] = av[i][j];
-            count++;
-        }
-        chara[count] = '\n';
-        count++;
-    }
-    chara[count]='\0';
-    return(chara);
+	if (ac == 0 && av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+		size++;
+		chara = malloc(size * sizeof(char));
+		if (chara == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; av[i][j] != '\0'; i++)
+		{
+			chara[count] = av[i][j];
+			count++;
+		}
+		chara[count] = '\n';
+		count++;
+	}
+	chara[count] = '\0';
+	return (chara);
 }
 
