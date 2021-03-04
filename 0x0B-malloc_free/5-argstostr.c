aabae611f0866d9363ab574d1b56483b3d30b568
AnguiDev <angiedesarrollo@gmail.com>
2021-03-04 00:05:20 -0500

    exercise 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,31 +8,39 @@
 char *argstostr(int ac, char **av)
 {
     char *chara;
-    int i, j;
+    int i, j, size = 0, count = 0;
     
     if( ac == 0 && av == NULL)
     {
         return(NULL);
     }
-    if(!chara)
+    /*chara = malloc(sizeof(chara)*ac);
     {
-        return(NULL);
-    }
-    chara = malloc(sizeof(char)*ac);
-    {
-        if (chara == NULL)
-        {
-            return(NULL);
-        }
-    }
+       
+        
+    }*/
     for ( i = 0; i < ac; i++)
     {
         for ( j = 0; av[i][j] !='\0'; j++)
         {
+            size++;
         }
-        chara[i] = malloc(sizeof(char) * (j + 1));
-        
+        size++;
+
+        chara = malloc(size *sizeof(char));
+         if (chara == NULL)
+        {
+            return(NULL);
+        }
+        for ( i = 0; av[i][j] !='\0'; i++)
+        {
+            chara[count] = av[i][j];
+            count++;
+        }
+        chara[count] = '\n';
+        count++;
     }
-    return (chara);
- 
+    chara[count]='\0';
+    return(chara);
 }
+
