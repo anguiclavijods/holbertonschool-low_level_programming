03317746c5341268a2119ea122c74f661eb5cc40
AnguiDev <angiedesarrollo@gmail.com>
2021-03-05 18:58:16 -0500

    concat two arrays with malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *m;
+	int i = 0, j = 0;
+
+	for (; s1[i] != '\0'; i++)
+	{
+
+	}
+	for (; s2[j] != '\0'; j++)
+	{
+
+	}
+	m = (char*) malloc (sizeof(char) * (i + j + 1));
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		m[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		m[i] = s2[j];
+		j++;
+		i++;
+	}
+	i++;
+	m[i] = '\0';
+	return (m);
+}
