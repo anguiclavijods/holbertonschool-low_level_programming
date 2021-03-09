ddd0242658d80ee0832085c3f25e000ceff9ce7a
AnguiDev <angiedesarrollo@gmail.com>
2021-03-08 21:47:23 -0500

    test

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
+ *print_dog - name function
+ *@d: pointer a struct dog
  *
  */
 void print_dog(struct dog *d)
@@ -10,11 +11,11 @@ void print_dog(struct dog *d)
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
 
-	if(d == NULL)
+	if (d == NULL)
 	{
-		
+
 	}
-	if(d->name == NULL)
+	if (d->name == NULL)
 	{
 		printf("nil");
 	}
