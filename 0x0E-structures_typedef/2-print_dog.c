8a2f73ee25324949cf243b362da98ef4a3e4dd6e
AnguiDev <angiedesarrollo@gmail.com>
2021-03-09 10:26:51 -0500

    function that print nil that is value null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: ");
 		d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name);
-		printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == NULL ? printf("(nil)\n") : printf("Owner: %s\n", d->owner);
 	}
