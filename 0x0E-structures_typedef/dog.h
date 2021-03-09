eec569c7ea9b0424cc4282764b226018487cf5da
AnguiDev <angiedesarrollo@gmail.com>
2021-03-08 19:57:17 -0500

    crate struct for doggy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *dog - name struct
+ *struct dog - name struct
  *@name: name of doggy
- *@age: age of duggy
- *@owner: owner of duggy
+ *@age: age of doggy
+ *@owner: owner of doggy
  */
 struct dog
 {
