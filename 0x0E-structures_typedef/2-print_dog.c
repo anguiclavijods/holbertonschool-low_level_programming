c002500a2f04f7839835e0eaa75fa535f3ee19fe
AnguiDev <angiedesarrollo@gmail.com>
2021-03-09 22:20:22 -0500

    prints values or print nil if is null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,14 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d != NULL)
-	{
-		/*printf("Name: ");*/
+
 		d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		/*printf("Owner: ");*/
 		d->owner == NULL ? printf("(nil)\n") : printf("Owner: %s\n", d->owner);
-	}
+		if (d == NULL)
+			return;
 }
