035b543b831c57edadc9c313559a7727ae879d79
AnguiDev <angiedesarrollo@gmail.com>
2021-03-11 09:29:18 -0500

    function that search a integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && size <= 0 && cmp)
-		return(-1);
+	if (!array && size <= 0 && !cmp)
+		return (-1);
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
-			return(i);
+			return (i);
 		}
 	}
 	return (-1);
