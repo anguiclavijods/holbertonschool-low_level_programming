4710c5bf8fda756383ea5226235c073055665429
AnguiDev <angiedesarrollo@gmail.com>
2021-03-10 22:44:15 -0500

    function that execute parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - name to function
+ *@array: parameters of array
+ *@size: size of array
+ *@action: pointer of function name
+ *Return: none.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && size && action);
+/*printf("%d", *array);*/
+		return;
+}
