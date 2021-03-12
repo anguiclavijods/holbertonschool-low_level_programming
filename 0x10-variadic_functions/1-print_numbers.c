41b0bf3e5cd77e8260c193ad18b9e1d34bb5fa15
AnguiDev <angiedesarrollo@gmail.com>
2021-03-11 20:45:27 -0500

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_numbers - name function
+ *@separator: Pointer a char
+ *@n: Parameters
+ *Return: None.
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	/*unsigned int print_n;*/
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d\n", va_arg(ap, unsigned int));
+		else
+			printf("%d%s", va_arg(ap, unsigned int), separator);
+	}
+	va_end(ap);
+	return;
+}
