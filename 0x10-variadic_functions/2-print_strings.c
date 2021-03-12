6baffbf11b4305a473b9b792b3579c2dd1050c39
AnguiDev <angiedesarrollo@gmail.com>
2021-03-11 23:11:36 -0500

    function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,9 +10,9 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	char *cha;
 	va_list ap;
+	char *cha;
+	unsigned int i;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
