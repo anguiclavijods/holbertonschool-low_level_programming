59be3c28741cdd6e11c33a7c6c85e69ce1e51174
AnguiDev <angiedesarrollo@gmail.com>
2021-03-12 00:20:13 -0500

    print anyting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,10 +8,39 @@
  */
 void print_all(const char * const format, ...)
 {
-	const i;
 	va_list ap;
+	char *p_anything;
+	int i = 0;
 
 	va_start (ap, format);
-	printf = va_arg(ap, format);
+	while (format == NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				p_anything = va_arg(ap, char *);
+				if (p_anything == NULL)
+				{
+					printf("(nill)");
+					break;
+				}
+				break;
+		}
+		if (format[i+1] && (format[i] == 'c' ||
+		format[i] == 'i' || format[i] == 'f' ||
+		format[i] == 's'))
+		printf(", ");
+		i++;
+		
+	}
 	va_end(ap);
+	putchar('\n');
 }
