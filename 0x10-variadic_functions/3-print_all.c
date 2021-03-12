aa634eb1afc441cdafa89b7d97325fd4380db7b6
AnguiDev <angiedesarrollo@gmail.com>
2021-03-12 00:25:11 -0500

    print anyting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,44 +2,49 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- *
- *
- *
+ *print_all - print everyone name function
+ *@format: parameters
+ *Return: none
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	char *p_anything;
+	char *p_all;
 	int i = 0;
 
-	va_start (ap, format);
-	while (format == NULL && format[i] != '\0')
+	while (format == NULL)
+	{
+		putchar('\n');
+		return;
+	}
+	va_start(ap, format);
+	while (format[i])
 	{
 		switch (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			p_all = va_arg(ap, char *);
+			if (p_all == NULL)
+			{
+				printf("(nil)");
 				break;
-			case 'i':
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				break;
-			case 's':
-				p_anything = va_arg(ap, char *);
-				if (p_anything == NULL)
-				{
-					printf("(nill)");
-					break;
-				}
-				break;
+			}
+			printf("%s", p_all);
+			break;
 		}
-		if (format[i+1] && (format[i] == 'c' ||
-		format[i] == 'i' || format[i] == 'f' ||
-		format[i] == 's'))
-		printf(", ");
+		if (format[i + 1] && (format[i] == 'c' ||
+			format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+			printf(", ");
 		i++;
-		
 	}
 	va_end(ap);
 	putchar('\n');
