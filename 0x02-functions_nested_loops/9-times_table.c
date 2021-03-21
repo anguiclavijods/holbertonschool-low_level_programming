c8c0a59d59ea7b0f9f411b1cf4908987db68442b
AnguiDev <angiedesarrollo@gmail.com>
2021-03-21 14:40:18 -0500

    correction identation

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,32 @@
 */
 void times_table(void)
 {
-         int table, number;
-	 
-         for (table = 0 ; table <= 9; table++)
-         {
-                 for (number = 0; number <= 9 ; number++)
-                 {
-                         int times = table * number;
-                         if (times < 10)
-                         {
-                                 if (number > 0)
-                                 {
-                                         _putchar(' ');
-                                         _putchar(' ');
-                                 }
-                                 _putchar('0' + times);
-                         }
-                         else
-                         {
-                                 _putchar(' ');
-                                 _putchar('0' + (times / 10));
-                                 _putchar('0' + (times % 10));
-                         }
-                         if (number < 9)
-                                 _putchar(',');
-                 }
-                 _putchar('\n');
-         }
+	int table, number;
+
+	for (table = 0; table <= 9; table++)
+	{
+		for (number = 0; number <= 9; number++)
+		{
+			int times = table * number;
+
+			if (times < 10)
+			{
+				if (number > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + times);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + (times / 10));
+				_putchar('0' + (times % 10));
+			}
+			if (number < 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
 }
