d034f95a1e48061f19b73ccd08c03aa7a1e06749
AnguiDev <angiedesarrollo@gmail.com>
2021-03-21 16:56:13 -0500

    correction the file and logic

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,24 @@
 */
 int fib(int n)
 {
-/* Declare an array to store Fibonacci numbers. */
-	int f[n + 2]; /* 1 extra to handle case, n = 0*/
-	int i;
+	int n1, n2, i;
 
-/* 0th and 1st number of the series are 0 and 1*/
-	f[0] = 0;
-	f[1] = 1;
-
-	for (i = 2; i <= n; i++)
-	{
-/* Add the previous 2 numbers in the series and store it */
-		f[i] = f[i - 1] + f[i - 2];
-	}
-	return (f[n]);
+	if (fib <= 0)
+		print("Please enter a positive integer")
+	else if (fib == 1)
+		print("Fibonacci sequence upto", fib, ":")
+		print(n1)
+	else
+		print("Fibonacci squence:")
+			while (i < fib)
+			{
+				print(n1);
+				nth = n1 + n2;
+				n1 = n2;
+				n2 = nth;
+				i += 1;
+			}
+	return (0);
 }
 /**
  *main - function main
