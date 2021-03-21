901eacbd1eb059632468ac2bacf1a6b19d4913e3
AnguiDev <angiedesarrollo@gmail.com>
2021-03-21 08:28:50 -0500

    correction exercise for identation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
- 
 #include <stdio.h>
 #include <unistd.h>
 /**
@@ -8,6 +7,7 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDERR_FILENO,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
