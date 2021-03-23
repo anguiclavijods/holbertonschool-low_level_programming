626356a4e8e40f41ed20bae1b809de768caa4e2d
AnguiDev <angiedesarrollo@gmail.com>
2021-03-23 10:23:36 -0500

    comments of the functions

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 /**
- * 
- * 
+ * print_listint - Name function for pint list
+ * @h: pointer to arguments
+ * Return: value of all elements
  */
 size_t print_listint(const listint_t *h)
 {
 int i = 0;
 
-while(h != NULL)
+while (h != NULL)
 {
-	printf ("%d\n", h->n);
+	printf("%d\n", h->n);
 	h = h->next;
 	i++;
 }
