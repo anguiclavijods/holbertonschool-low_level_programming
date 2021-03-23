5fb2ce398bd93f8e9f6b1561c1f4226a4db0ada4
AnguiDev <angiedesarrollo@gmail.com>
2021-03-23 18:58:20 -0500

    add elements of lists to the begining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
- * 
- * 
+ * add_nodeint - function that add nodes at the begging list
+ * @head: pointer to elements of list
+ * @n: pointer to elements integers
+ * Return: Value of new element of list
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
