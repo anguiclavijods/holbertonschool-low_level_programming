4e5731500ad802a4f0f0f9a7f021feaf14c7d707
AnguiDev <angiedesarrollo@gmail.com>
2021-03-23 23:41:54 -0500

    function that free memory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that free memory of list
+ * @head: pointer to dates of struct
+ *Return: none 
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new, *aux = NULL;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	aux = *head;
+	while (aux)
+	{
+		new = aux->next;
+		free(aux);
+		aux = new;
+	}
+	*head = NULL;	
+}
