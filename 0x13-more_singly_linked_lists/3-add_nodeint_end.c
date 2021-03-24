e342701df96029d370cdeb3eddbb8f68f9029b1c
AnguiDev <angiedesarrollo@gmail.com>
2021-03-23 19:55:56 -0500

    add elements of list to the end of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - function that add nodes to the end list
+ *@head:  pointer to elements of head
+ *@n: entry parameters integers
+ *Return: The nodes the final list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *aux;
+
+	new = (listint_t *)malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		aux->next = new;
+	}
+	return (*head);
+
+}
