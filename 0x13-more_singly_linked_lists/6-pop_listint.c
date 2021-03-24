82a83038e05e54889bd981de19cf379fb3bfb642
AnguiDev <angiedesarrollo@gmail.com>
2021-03-24 11:34:27 -0500

    function that deletes data node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * pop_listint - function that deletes 
- * 
- * 
+ * pop_listint - function that deletes the date head node
+ * @head: pointer to the linst linked
+ *Return: data of the head
  */
 int pop_listint(listint_t **head)
 {
 	int data = 0;
 	listint_t *temp;
 
-	if (data =(*head)->next)
+	if (*head)
 	{
 		data = (*head)->n;
 		temp = (*head)->next;
-		free (*head);
+		free(*head);
 		*head = temp;
 		/*(*head)->n=NULL;*/
 	}
 	return (data);
-
 }
