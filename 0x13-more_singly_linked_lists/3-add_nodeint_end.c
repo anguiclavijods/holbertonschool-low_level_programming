7a3d5d0cd79edd31beb99adcbdbca5bf19276387
AnguiDev <angiedesarrollo@gmail.com>
2021-03-23 20:06:44 -0500

    add end the list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *aux;
 
-	new = (listint_t*)malloc(sizeof(listint_t));
+	new = (listint_t *)malloc(sizeof(listint_t));
 	new->n = n;
 	new->next = NULL;
 
