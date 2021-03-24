9ff45623ec93a64c6ecc3fa98ccb1de2aa40374b
AnguiDev <angiedesarrollo@gmail.com>
2021-03-24 12:50:03 -0500

    function that return of nth of head

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - name of function that return nth of head
+ * @head: pointer to linked list
+ * @index: ponter to position linked list
+ * Return:  values of head
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+	while (i != index)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
