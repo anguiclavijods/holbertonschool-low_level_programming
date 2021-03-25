77b7a8ff9b63e79f968e2ee819d2b0efe92f84d0
AnguiDev <angiedesarrollo@gmail.com>
2021-03-24 23:30:08 -0500

    function that insert to new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * 
- * 
- * 
+ * insert_nodeint_at_index - name of function that insert value
+ * @head: pointer a header of list
+ * @idx: indice of list
+ * @n: parameters of entry
+ * Return: value of parameter insert
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *aux;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (!head)
 	{
 		return (NULL);
 	}
+	if (idx != 0)
+	{
+		aux = *head;
+		while (aux->next)
+		{
+			if (i == idx)
+			{
+				aux = aux->next;
+			}
+			else
+			{
+				return (NULL);
+			}
+			i++;
+		}
+	}
 
-	new=malloc(sizeof(listint_t));
-	if(!idx)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 	{
 		return (NULL);
 	}
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next= *head;
+		new->next = *head;
 		*head = new;
-		return(new);
+		return (new);
 	}
-	aux= *head;
-	while (aux->next)
-	{
-		if(i == idx)
-		{
-			new->next = aux->next;
-			aux->next = aux;
-			return (new);
-		}
-		aux = aux->next;
-		i++;
-	}
-
+	new->next = aux->next;
+	aux->next = aux;
+	return (new);
 }
