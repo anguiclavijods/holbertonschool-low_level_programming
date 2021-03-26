67744dd3b3cf2c9b015d9e3522727684297d13d8
AnguiDev <angiedesarrollo@gmail.com>
2021-03-26 00:20:23 -0500

    function that print hexadeximal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,35 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *print_binary - function that print binarys
+ *@n: entry point of parameters
+ *Return: none
  */
 void print_binary(unsigned long int n)
 {
-	if (n == NULL)
+
+	int rem, count = 1, step = 1;
+
+	if (n == NULL || n == 1)
 	{
-		return (0);
+		_putchar(n + '0');
+		return;
+	}
+
+	while (n >> count != 0 && count < 64)
+	{
+		count++;
+	}
+	count--;
+	while (count >= 0)
+	{
+		if (( n >> count & 1) == 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		count--;
 	}
 }
