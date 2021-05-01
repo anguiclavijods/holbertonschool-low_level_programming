12965d249ceebe5cd519c53182ea171917fc4e4b
AnguiDev <angiedesarrollo@gmail.com>
2021-05-01 15:17:03 -0500

    print nodes of list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,6 +4,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     /*verify if h exist*/
+    if (h == NULL)
+    {
+        return(NULL);
+    }
     while (h != NULL)
     {
         printf ("%d\n", h->n);
