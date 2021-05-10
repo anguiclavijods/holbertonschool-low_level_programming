a1f17cd6821896e9774e0aab3784dc6735db8791
AnguiDev <angiedesarrollo@gmail.com>
2021-05-10 16:42:44 -0500

    get bit in list

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * get_bit - function that verify one node
+ * @n: node to verify
+ * @index: index of list bits
+ * Return: None
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index == NULL)
+	unsigned log int result;
+	result = n & (unsigned long)(1 << index);
+	if (result > 0)
 	{
-		return (0);
+		return (1);
 	}
 }
