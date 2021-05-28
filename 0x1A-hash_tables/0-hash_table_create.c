c80e40ee7a68201b3ebcb25a33d856438b9b3d4d
AnguiDev <angiedesarrollo@gmail.com>
2021-05-28 16:31:20 -0500

    created new table or copy

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - Function that created to new table
+ * @size - Parameter that receive function with size of 1024
+ * Return: New hash table or  copy of hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	/*create new table with struct hash_table_t, initialize in 0 for valgrind*/
+	hash_table_t *new_hash_t = NULL;
+
+	new_hash_t = malloc(sizeof(hash_table_t));/*reserve memory heap*/
+
+	/*edge case if its is null or fail*/
+	if (new_hash_t == NULL)
+	{
+		return (NULL);
+	}
+
+	/*start the new hash with nodes*/
+	new_hash_t->size = size;
+	new_hash_t->array = calloc(size, sizeof(hash_node_t));
+
+	/*if the new hash table, the array is NULL or 0*/
+	if (new_hash_t->array == NULL)
+	{
+		return (NULL);
+	}
+
+	/*return new hash table*/
+	return (new_hash_t);
+}
