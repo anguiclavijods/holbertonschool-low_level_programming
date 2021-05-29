28604f132bc82d3f9eae085f695b927b07fffa01
AnguiDev <angiedesarrollo@gmail.com>
2021-05-28 20:19:01 -0500

    get key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * key_index - Function that get index of parameter
+ * @key: Parameter string
+ * @size: Parameter for size
+ * Return: The mod of size and new hash this is index
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	/*validate if parameter is null*/
+	if (size == 0)
+	{
+		return ('\0');
+	}
+
+	/*declare the var to return*/
+	unsigned long int new_hash;
+
+	/*called function hash_djbs2 for paremeter key*/
+	new_hash = hash_djb2(key);
+
+	/*var with mod size*/
+	return (new_hash %= size);
+}
