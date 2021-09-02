86ebf4211013f3f1150873a108e04e135d068b7f
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-01 20:53:46 -0500

    Algorithm that search on value specific and error if not find value

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
+/**
+* linear_search - verify the list of values for search value specify
+* @array: array of integers where search
+* @size: size of array
+* @value: value of search
+* Return: return on integer if value exits otherwise return -1 if not exists
+*/
 
+int linear_search(int *array, size_t size, int value)
+{
+	size_t iter;
 
-int linear_search(int *array, size_t size, int value);
+	if (!array)
+		return (-1);
+	for (iter = 0; iter < size; iter++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", iter, array[iter]);
+		if (array[iter] == value)
+			return (iter);
+	}
+	return (-1);
+}
